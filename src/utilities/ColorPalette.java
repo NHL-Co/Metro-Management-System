80efdfc8e3c8f8234c4db448e95b7f2d798c9c02
Laiba Ijaz <125128708+lai-jaz@users.noreply.github.com>
2024-12-07 12:51:01 +0500

    Update ColorPalette.java
    Fixed light grey

--- src/utilities/ColorPalette.java
+++ src/utilities/ColorPalette.java
@@ -6,5 +6,5 @@ import java.awt.Color;
 public class ColorPalette {
     public static Color YELLOW = Color.decode("#fac31c");
     public static Color BLUE = Color.decode("#014171");
-    public static Color LIGHT_GREY = Color.decode("#014171");
+    public static Color LIGHT_GREY = Color.decode("#d3d3d3");
 }
