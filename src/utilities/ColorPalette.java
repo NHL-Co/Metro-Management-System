ec25d8faf9c173e0436884dc3da2d0abb0804226
Laiba Ijaz <125128708+lai-jaz@users.noreply.github.com>
2024-12-08 01:18:46 +0500

    Update ColorPalette.java
    Added new color for hovering

--- src/utilities/ColorPalette.java
+++ src/utilities/ColorPalette.java
@@ -6,5 +6,6 @@ import java.awt.Color;
 public class ColorPalette {
     public static Color YELLOW = Color.decode("#fac31c");
     public static Color BLUE = Color.decode("#014171");
+    public static Color HOVER_BLUE = Color.decode("#01233d");
     public static Color LIGHT_GREY = Color.decode("#d3d3d3");
 }
