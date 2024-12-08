14b5e666c7f2bb17c4a5dcdf1c10fed52e93f88b
Laiba Ijaz <125128708+lai-jaz@users.noreply.github.com>
2024-12-08 16:42:55 +0500

    Update and rename product.sql to sample_inserts.sql
    Added sample rows

---
+++ SQL/sample_inserts.sql
@@ -0,0 +1,28 @@
+-- Data insertion for `vendor` table
+INSERT INTO `vendor` (`vendor_id`, `name`, `cnic`, `phone_number`) VALUES
+(1, 'Traders ABC', '2314563241456', '27654342312'),
+(2, 'XYZ Traders', '4857384783421', '34562345678');
+
+-- Data insertion for `product` table
+INSERT INTO `product` (`product_id`, `vendor_id`, `name`, `category`, `original_price`, `sale_price`, `price_by_carton`) VALUES
+(1, 1, 'Herbal Essences Shampoo', 'Personal Care', 800, 1000, 6000),
+(2, 1, 'Safeguard Pure White', 'Personal Care', 20, 40, 200),
+(3, 1, 'Nerds', 'Food and Beverages', 200, 250, 1100),
+(4, 1, 'Haribo Goldbears', 'Food and Beverages', 350, 400, 2400),
+(5, 2, 'Bananas', 'Food and Beverages', 100, 110, 2000),
+(6, 1, 'Lotte Choco Pie', 'Food and Beverages', 40, 45, 200),
+(7, 2, 'Apples', 'Food and Beverages', 290, 300, 3000),
+(8, 1, 'Colgate toothpaste', 'Personal Care', 38, 40, 1000),
+(9, 1, 'Adam\'s Mozarella Cheese', 'Food and Beverages', 450, 480, 2000);
+
+-- Data insertion for `product_branch` table
+INSERT INTO `product_branch` (`product_id`, `branch_code`, `quantity`) VALUES
+(1, '123LHE', 100),
+(2, '123LHE', 70),
+(3, '123LHE', 100),
+(4, '123LHE', 50),
+(5, '123LHE', 100),
+(6, '123LHE', 100),
+(7, '123LHE', 100),
+(8, '123LHE', 200),
+(9, '123LHE', 50);
